8b87dbc30e24a976bbcd5611aa16984e5efaac2a
alexandresmagghe <alexandre-smagghe@outlook.com>
2018-06-01 22:19:35 +0200

    Structure of Wireframe Desktop

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+function scrollArt() {
+  var element = document.getElementById("artiste");
+  element.classList.add("scroll-artiste");
+
+};
+
+function scrollEnt() {
+  var element = document.getElementById("entreprise");
+  element.classList.add("scroll-entreprise");
+
+};
+
+function closeArtEnt() {
+  var element = document.getElementById("artiste");
+  element.classList.remove("scroll-artiste");
+  var elementTwo = document.getElementById("entreprise");
+  elementTwo.classList.remove("scroll-entreprise");
+
+
+}
