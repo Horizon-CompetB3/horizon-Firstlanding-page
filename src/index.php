c9fa0784c3c7a2901d74c28e9c31c70664aa854c
alexandresmagghe <alexandre-smagghe@outlook.com>
2018-06-03 19:40:28 +0200

    add confirm's modal

--- src/index.php
+++ src/index.php
@@ -1,3 +1,45 @@
+<?php
+
+
+include "inc/init.inc.php";
+if(isset($_POST['ent-envoyer'])){
+
+if(!filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL))
+{
+  echo '<script>var element = document.getElementById("modale-ent");
+  element.classList.add("modale-ent-visible");
+</script>';
+}else {
+
+    $requeteInsertion = $pdo->prepare('INSERT INTO entmail (name, mail) VALUES (:name, :mail)');
+    $requeteInsertion->bindValue('mail', $_POST['mail'], PDO::PARAM_STR);
+    $requeteInsertion->bindValue('name', $_POST['name'], PDO::PARAM_STR);
+    $requeteInsertion->execute();
+    echo '<div class="mod-confirme"><h3>Votre Email a bien été enregistré</h3></div>';
+
+
+  }
+}
+
+if(isset($_POST['envoyer'])){
+
+if(!filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL))
+{
+  echo '<script>function modal(){var element = document.getElementById("modale-ent");
+  element.classList.add("modale-ent-visible")};
+</script>';
+}else {
+
+    $requeteInsertion = $pdo->prepare('INSERT INTO mail (name, mail) VALUES (:name, :mail)');
+    $requeteInsertion->bindValue('mail', $_POST['mail'], PDO::PARAM_STR);
+    $requeteInsertion->bindValue('name', $_POST['name'], PDO::PARAM_STR);
+    $requeteInsertion->execute();
+    echo '<div class="mod-confirme"> <h3>Votre Email a bien été enregistré</h3></div>';
+
+
+  }
+}
+?>
 <!doctype html>
 <html class="no-js" lang="">
 
@@ -5,16 +47,18 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Banan'Art</title>
-  <meta name="description" content="">
+  <meta name="description" content="Banna'Art permet aux entreprises de recycler leurs rebuts tout en permettant aux artistes de se fournir en matières premières">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
+  <meta name="author" content="Horizon">
+  <meta name="copyright" content="Horizon">
 </head>
 
 <body>
 <section id="section-first">
   <header>
-    <div class="logo-bananart"><img src="img/logo_bananart_blanc.png"></div>
+    <div class="logo-bananart"><img src="img/logo_bananart_blanc.png" alt="logo"></div>
       <div class="titre"><h1><span>Banan'Art</span>, soutenir l'art <br/>sans gaspillage</h1></div>
   </header>
   <div id="cont-intro-text">
@@ -28,29 +72,32 @@
 <section id="artiste" class="artiste">
 
   <div class="div-photo">
-    <img src="">
+    <img src="img/artiste.jpg">
   </div>
   <div class="div-content">
     <button class="close" onclick="closeArtEnt()">X</button>
     <div class="div-text">
       <h2>Cherchez l’inspiration, on cherche les matériaux</h2>
       <div class="div-description">
-        <p>Artiste, vous fournir en matières premières vous coûtent 
+        <p>Artiste, vous fournir en matières premières vous coûtent
         cher et vous prend du temps ?<br/>
         En récupérant des matériaux auprès des entreprises, Banan'Art  vous fournit en matières premières de toutes sortes, rapidement et à moindre coût.<br/><br/><br/>
         Tenez-vous au courant en vous inscrivant à la Newsletter !</p>
       </div>
-        <div class="formulaire">
-      <input class="input-text" placeholder="Nom" type="text"></br>
-      <input class="input-text" placeholder="email" type="text"></br>
-      <input class="submit" type="submit">
-      </div>
+      <form method="post" action="" class="formulaire">
+        <input type="text" class="input-text" id="nameInput" placeholder="nom" name="name">
+        <input type="text" class="input-text" id="mailInput" placeholder="Adresse email" name="mail"><br>
+
+        <input class="submit" type="submit" name="envoyer" value="ENVOYER">
+
+      </form>
 
       <div class="reseaux">
-        <a href=""><img src="img/twitter.png" alt="twitter" class="icon-reseaux"></a>
-        <a href=""><img src="img/facebook.png" alt="facebook" class="icon-reseaux"></a>
-        <a href=""><img src="img/instagram.png" alt="instagram" class="icon-reseaux"></a>
-        <a href=""><img src="img/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
+        <a href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
+
       </div>
       <div>
         <p class="little-text">Banna'Art est seule destinataire des informations saisies, qui ne seront pas commercialisées à des tiers. Conformément au RGPD, vous disposez d’un droit d’accès, de modification, de rectification et de suppression de ces données. Pour l’exercer, vous pouvez envoyer un e-mail à l’adresse suivante : bananart.entreprise@gmail.com</p></div>
@@ -61,30 +108,32 @@
 <section id="entreprise" class="entreprise">
 
   <div class="div-photo">
-    <img src="">
+    <img src="img/entreprise.png">
   </div>
   <div class="div-content">
     <button class="close" onclick="closeArtEnt()">X</button>
     <div class="div-text">
       <h2>Devenez mécène sans même dépenser </h2>
       <div class="div-description">
-        <p>Votre entreprise génère des rebuts ? 
+        <p>Votre entreprise génère des rebuts ?
         Leurs gestions représentent un coût et une perte de temps ?
         Faites du recyclage ou encore mieux : de l’upcyclage !<br/>
-        En cédant vos rebuts vous faciliterai ainsi le travail des artistes tout en vous débarrassant de vos rebuts et surplus.<br/><br/><br/>
+        En cédant vos rebuts vous faciliterez ainsi le travail des artistes tout en vous débarrassant de vos rebuts et surplus.<br/><br/><br/>
         Tenez-vous au courant en vous inscrivant à la Newsletter !</p>
       </div>
-      <div class="formulaire">
-      <input class="input-text" placeholder="Nom" type="text"></br>
-      <input class="input-text" placeholder="email" type="text"></br>
-      <input class="submit" type="submit">
-      </div>
+      <form method="post" action="" class="formulaire">
+        <input type="text" class="input-text" id="nameInput" placeholder="nom" name="name">
+        <input type="text" class="input-text" id="mailInput" placeholder="Adresse email" name="mail"> <br>
+
+        <input class="submit" type="submit" name="ent-envoyer" value="ENVOYER">
+
+      </form>
 
       <div class="reseaux">
-        <a href=""><img src="img/twitter.png" alt="twitter" class="icon-reseaux"></a>
-        <a href=""><img src="img/facebook.png" alt="facebook" class="icon-reseaux"></a>
-        <a href=""><img src="img/instagram.png" alt="instagram" class="icon-reseaux"></a>
-        <a href=""><img src="img/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
+        <a href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
+        <a href=""><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
 
       </div>
       <div>
@@ -93,7 +142,9 @@
   </div>
 
 </section>
-
+<div id="modale-ent" class="modale-ent">
+  <h3>Adresse incorrecte Mail incorrect</h3>
+</div>
 <script src="js/main.js"></script>
 
 </body>
