42242b691b4d0d12b83be2ec0709b61547ba3114
Alexandre Smagghe <alex.s95120@gmail.com>
2018-06-06 23:36:14 +0200

    Css Adjustements

--- src/index.php
+++ src/index.php
@@ -93,10 +93,10 @@ if(!filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL))
       </form>
 
       <div class="reseaux">
-        <a href=""><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
-        <a href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
-        <a href=""><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
-        <a href=""><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.linkedin.com/company/banan-art/"><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.instagram.com/bananartaetco/"><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
+        <a target="_blank" href="https://twitter.com/Bananartetco"><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
 
       </div>
       <div>
@@ -132,10 +132,10 @@ if(!filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL))
       </form>
 
       <div class="reseaux">
-        <a href=""><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
-        <a href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
-        <a href=""><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
-        <a href=""><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.linkedin.com/company/banan-art/"><img src="img/rs/twitter.png" alt="twitter" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.facebook.com/bananartetco/"><img src="img/rs/facebook.png" alt="facebook" class="icon-reseaux"></a>
+        <a target="_blank" href="https://www.instagram.com/bananartaetco/"><img src="img/rs/instagram.png" alt="instagram" class="icon-reseaux"></a>
+        <a target="_blank" href="https://twitter.com/Bananartetco"><img src="img/rs/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
 
       </div>
       <div>
