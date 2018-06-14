880fd70dbb870a516c6fc87d4b2f209c66ca4a8c
alexandresmagghe <alexandre-smagghe@outlook.com>
2018-06-14 14:30:56 +0200

    rework landing v3

--- src/inc/rs.inc.php
+++ src/inc/rs.inc.php
@@ -1,7 +1,7 @@
 <div class="reseaux">
-  <a target="_blank" href=""><img src="img/twitter-noir.svg" alt="twitter" class="icon-reseaux"></a>
-  <a target="_blank" href="https://www.facebook.com/Lugh-art-sur-commande-en-entreprise-567883830278473/"><img src="img/facebook-noir.svg" alt="facebook" class="icon-reseaux"></a>
-  <a target="_blank" href=""><img src="img/instagram-noir.svg" alt="instagram" class="icon-reseaux"></a>
-  <a target="_blank" href=""><img src="img/linkedin-noir.svg" alt="linkedin" class="icon-reseaux"></a>
+  <a target="_blank" href=""><img src="img/twitter.png" alt="twitter" class="icon-reseaux"></a>
+  <a target="_blank" href="https://www.facebook.com/Lugh-art-sur-commande-en-entreprise-567883830278473/"><img src="img/facebook.png" alt="facebook" class="icon-reseaux"></a>
+  <a target="_blank" href=""><img src="img/instagram.png" alt="instagram" class="icon-reseaux"></a>
+  <a target="_blank" href=""><img src="img/linkedin.png" alt="linkedin" class="icon-reseaux"></a>
 
 </div>
